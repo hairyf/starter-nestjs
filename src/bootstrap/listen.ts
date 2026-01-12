0351078673b1dc122fe1c688cc5c98dbb9386bc6
hairyf <wwu710632@gmail.com>
2026-01-12 16:50:52 +0800

    chore: update package scripts to use oxrun, add oxrun dependency, and replace chalk with styleText for logging

--- src/bootstrap/listen.ts
+++ src/bootstrap/listen.ts
@@ -1,18 +1,19 @@
 import type { INestApplication } from '@nestjs/common'
 import process from 'node:process'
+import { styleText } from 'node:util'
 import { Logger } from '@nestjs/common'
-import chalk from 'chalk'
 
 export async function withNestjsListen(app: INestApplication, port: string | number = process.env.SERVER_PORT || 3000) {
   const logger = new Logger('Listen')
 
   try {
     await app.listen(port)
-    logger.log(`${chalk.bold('Listening on:')} ${chalk.gray(`http://127.0.0.1:${port}`)}`)
-    logger.log(`${chalk.bold('Swaggier URL:')} ${chalk.gray(`http://127.0.0.1:${port}/swagger/json`)}`)
-    logger.log(`${chalk.bold('Swaggier URL:')} ${chalk.gray(`http://127.0.0.1:${port}/swagger/website`)}`)
+
+    logger.log(`${styleText('bold', 'Listening on:')} ${styleText('gray', ` http://127.0.0.1:${port}`)}`)
+    logger.log(`${styleText('bold', 'Swaggier JSON:')} ${styleText('gray', `http://127.0.0.1:${port}/swagger/json`)}`)
+    logger.log(`${styleText('bold', 'Swaggier URL:')} ${styleText('gray', ` http://127.0.0.1:${port}/swagger/website`)}`)
     process.env.NODE_ENV
-    && logger.log(`${chalk.bold('Environments:')} ${chalk.gray(process.env.NODE_ENV)}`)
+    && logger.log(`${styleText('bold', 'Environments:')} ${styleText('gray', process.env.NODE_ENV)}`)
   }
   catch (error: any) {
     if (error.code !== 'EADDRINUSE')
