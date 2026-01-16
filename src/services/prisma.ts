847266bad6b02992f2dedc52b4f99ff2747f72d0
hairyf <wwu710632@gmail.com>
2026-01-16 09:09:55 +0800

    feat: remove lnv configuration, add Prisma migration files, and integrate MariaDB adapter

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,10 +1,19 @@
 import process from 'node:process'
 import { proxy } from '@hairy/utils'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import { PrismaClient } from '@prisma/client'
 
 export const prisma = proxy<PrismaClient>()
 
-if (process.env.DATABASE_URL)
-  prisma.proxy.update(new PrismaClient({ accelerateUrl: process.env.DATABASE_URL }))
+if (process.env.DATABASE_URL) {
+  const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5,
+  })
+  prisma.proxy.update(new PrismaClient({ adapter }))
+}
 
 export const isPrismaAvailable = prisma.proxy.original() !== undefined
